2db23c0bc072f5bd9933d87abb2df06ceec347d4
looser88 <98893135+looser88@users.noreply.github.com>
2024-03-10 18:51:51 +0530

    Update practice.py

--- plugins/practice.py
+++ plugins/practice.py
@@ -1,10 +1,12 @@
-
-
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto, ForceReply
 from pyrogram import Client, filters, enums
 from bot import Bot
 import asyncio
 
+
+
+#############reply any massege from a group
+##   (filters.group(coustom chat.id) & filters.text(eg - hi) & filters.incoming)
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     button = [[InlineKeyboardButton(text="ᴄᴏɴᴛᴀᴄᴛ ᴀᴅᴍɪɴ", url=f"https://t.me/alexa_search_bot")]]
